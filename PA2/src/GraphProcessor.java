1a3e95101484c03dc56c30b01240dc7ce9d761c8
Gabby Ortman <ortmangl@gmail.com>
2017-11-13 20:51:50 -0600

    add bfs path

--- PA2/src/GraphProcessor.java
+++ PA2/src/GraphProcessor.java
@@ -1,10 +1,17 @@
 import java.util.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class GraphProcessor {
 	
-	public GraphProcessor(String graphData) { 
-		
+	public WebGraph graph;
+	private int num_vertices;
+	private ArrayList<HashSet<String>> strongly_connected_components;
+	private HashSet<String> scc_visited = new HashSet<String>();
+	
+	public GraphProcessor(String graphData) throws FileNotFoundException { 
+		this.graph = new WebGraph();
+		constructGraph(graphData);
 	}
 	
 	/**
@@ -24,8 +31,46 @@ public class GraphProcessor {
 	 * @return ArrayList of strings repr bfs path from vertex u to vertex v.
 	 */
 	public ArrayList<String> bfsPath(String u, String v) { 
-		
-		return null; 
+	    Queue<String> bfs_queue = new ArrayDeque<>();
+	    HashSet<String> visited = new HashSet<String>();
+	    HashMap<String, String> parent = new HashMap<String, String>();
+	    
+	    bfs_queue.add(u);
+	    visited.add(u);
+	    parent.put(u, null);
+	    
+	    // while queue is not empty
+	    while(!bfs_queue.isEmpty()) { 
+	    	String current = bfs_queue.remove();
+	    	ArrayList<String> edges = graph.getAdjacencyMatrix().get(current);
+	    	// for each edge, w, current -> w
+	    	for(String edge : edges) { 
+	    		// if edge is unvisited
+	    		if(!visited.contains(edge)) { 
+	    			parent.put(edge, current);
+	    			bfs_queue.add(edge);
+	    			visited.add(edge);
+	    		}
+	    	}
+	    }
+	    
+	    String path_vertex = parent.get(v);
+	    if(path_vertex == null) { 
+	    	return new ArrayList<String>();
+	    }
+	    
+	    ArrayList<String> rev_path = new ArrayList<String>();
+	    rev_path.add(v);
+	    while(path_vertex != null) { 
+	    	rev_path.add(path_vertex);
+	    	path_vertex = parent.get(path_vertex);
+	    }
+	    
+	    ArrayList<String> path = new ArrayList<String>();
+	    for(int i = rev_path.size() - 1; i >= 0; i--) { 
+	    	path.add(rev_path.get(i));
+	    }
+	    return path;
 	}
 	
 	/**
@@ -52,5 +97,34 @@ public class GraphProcessor {
 	public int centrality(String v) { 
 		
 		return 0;
+	}
+	
+	private void constructGraph(String graphData) throws FileNotFoundException { 
+		File f = new File(graphData);
+		Scanner s = new Scanner(f);
+		
+		this.num_vertices = s.nextInt();
+		s.nextLine();
+		
+		while(s.hasNextLine()) { 
+			String vertex_edge = s.nextLine();
+			processVertexEdgeLine(vertex_edge);
+		}
+		s.close();
+	}
+	
+	private void processVertexEdgeLine(String vertex_edge) { 
+		Scanner line_scanner = new Scanner(vertex_edge);
+		String vertex = line_scanner.next();
+		String edge = line_scanner.next();
+		
+		this.graph.addVertex(vertex);
+		this.graph.addEdge(vertex, edge);
+		line_scanner.close();
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException { 
+		GraphProcessor gp = new GraphProcessor("WikiComplexity.txt");
+		System.out.println(gp.bfsPath("/wiki/Dynamical_systems_theory", "/wiki/Biological_organisation").toString());
 	}
 }
